348603f75e7db3bb49c7e8567cf5bdd42c93b933
gnetscher <gnetscher@gmail.com>
2016-07-27 13:36:53 -0700

    Updated move to ignore hidden files

--- DailyMove.sh
+++ DailyMove.sh
@@ -1,5 +1,5 @@
-# copy files from the previous day to videoArchive without overwriting
-find '/volume1/video/' -mindepth 1 -type f -mtime -1 -exec cp -n '{}' '/volume1/videoArchive/' \;
+# copy files from the previous day to videoArchive without overwriting avoiding hidden folders and files
+find '/volume1/video/' -not -path '*/\.*' -mindepth 1 -type f -mtime -1 -exec cp -n '{}' '/volume1/videoArchive/' \;
 
 # remove files older than 3 days 
 find '/volume1/video/' -mindepth 1 -mtime +3 -exec rm -f '{}' \;
